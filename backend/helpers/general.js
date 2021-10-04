f54e51b32d01675c3f4b9bbd5cc9812993b686d5
leshawn-rice <leshawnrice3@gmail.com>
2021-10-03 18:38:25 -0700

    added general helpers for types

---
+++ backend/helpers/general.js
@@ -0,0 +1,27 @@
+function isString(str) {
+  return (typeof (str) === 'string');
+}
+
+function isNumber(num) {
+  return (typeof (num) === 'number');
+}
+
+function isBoolean(bool) {
+  return (typeof (bool) === 'boolean');
+}
+
+function isObject(obj) {
+  return (typeof (obj) === 'object');
+}
+
+function isArray(arr) {
+  return Array.isArray(arr);
+}
+
+module.exports = {
+  isString,
+  isNumber,
+  isBoolean,
+  isObject,
+  isArray
+}
