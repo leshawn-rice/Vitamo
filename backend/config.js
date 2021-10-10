56a8d511e6f723e9bf0b5c213abe0fe87a37bf59
leshawn-rice <leshawnrice3@gmail.com>
2021-10-10 16:44:10 -0700

    added api key to config

--- backend/config.js
+++ backend/config.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 // Obviously remove 'secret-dev' before this goes to production
 const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 const PORT = +process.env.PORT || 3001;
+const API_KEY = process.env.API_KEY;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
@@ -16,6 +17,7 @@ const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === 'test' ? 1 : 12;
 module.exports = {
   SECRET_KEY,
   PORT,
+  API_KEY,
   BCRYPT_WORK_FACTOR,
   getDatabaseUri,
 };
